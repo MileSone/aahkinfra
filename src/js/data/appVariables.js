e05eef2ef25acfa65d0793da7e6fbb75f7548e0b
MW-Mac <mingyao.zhu@oracle.com>
2018-12-15 19:14:29 +0800

    first commit

---
+++ src/js/data/appVariables.js
@@ -0,0 +1,23 @@
+define(['ojs/ojcore', 'jquery'], 
+    function (oj, $) {
+    /**
+     * The view model for the main content view template
+     */
+    function appVariablesModel() {
+        var self = this;
+        self.mcsLoginUser = "mile";
+        self.mcsLoginPassword = "Mcs@1234";
+        self.isLoggedIn= false;
+
+        self.appId = "com.oraclecorp.internal.ent3.apac.scc.aahkinfra";
+        self.appVersion = "1.0.0";
+        self.androidSenderID = "794617537610";
+        self.deviceToken = ""; // for notificaton
+        self.errMsg = "";
+        
+        self.response;
+        
+    }
+    
+    return new appVariablesModel();
+});
